7a4169a1bfa34f55f028f88548146d1df5525aa3
Ivan_Velichko <showman.velichko@gmail.com>
2021-10-05 16:03:00 +0300

    Added localization settings for error messages

--- rest-api-task-controller/src/main/java/com/epam/esm/config/SpringConfig.java
+++ rest-api-task-controller/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -1,7 +1,9 @@
 package com.epam.esm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,4 +17,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
+    @Bean
+    public ResourceBundleMessageSource getResourceBundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.addBasenames("/localization");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
--- rest-api-task-controller/src/main/java/com/epam/esm/exception/CustomControllerAdvisor.java
+++ rest-api-task-controller/src/main/java/com/epam/esm/exception/CustomControllerAdvisor.java
@@ -1,5 +1,7 @@
 package com.epam.esm.exception;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 /**
@@ -17,6 +21,15 @@ import java.util.Optional;
 
 @ControllerAdvice
 public class CustomControllerAdvisor {
+    private ResourceBundleMessageSource resourceBundleMessageSource;
+    private static final String ENTITY_NOT_FOUND = "error.entity.not.found.";
+    private static final List<String> AVAILABLE_LOCALES = Arrays.asList("en_US", "ru_RU");
+    private static final Locale DEFAULT_LOCALE = new Locale("en", "US");
+
+    @Autowired
+    public CustomControllerAdvisor(ResourceBundleMessageSource resourceBundleMessageSource) {
+        this.resourceBundleMessageSource = resourceBundleMessageSource;
+    }
 
     @ExceptionHandler(NoSuchEntityException.class)
     public ResponseEntity<CustomResponse> handleNoSuchEntityException(NoSuchEntityException e) {
@@ -25,35 +38,35 @@ public class CustomControllerAdvisor {
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<CustomResponse> handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
+    public ResponseEntity<CustomResponse> handleEmptyResultDataAccessException(EmptyResultDataAccessException e, Locale locale) {
+        String msg = getEntityNameByMsg(e, ".", "Dao").toLowerCase(Locale.ROOT);
+        String localeMsg = resolveResourceBundle(ENTITY_NOT_FOUND + msg, locale);
         CustomResponse response = new CustomResponse(
-                getEntityNameByMsg(e, ".", "Dao") + " is not found.", 40401);
+                localeMsg, 40401);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
 
     private String getEntityNameByMsg(Exception e, String beforeName, String afterName) {
-        String text = msgFromStack(e);
-        int temp = text.indexOf(beforeName);
-        int index;
-        while (true) {
-            index = text.indexOf(beforeName, temp + 1);
-            if (index == -1) {
-                break;
-            }
-            temp = index;
-        }
-        int dao = text.indexOf(afterName, temp);
-        return text.substring(temp + 1, dao);
+        String text = msgFromStack(e, afterName);
+        return text.substring(text.lastIndexOf(beforeName) + 1, text.indexOf(afterName));
     }
 
-    private String msgFromStack(Exception e) {
+    private String msgFromStack(Exception e, String afterName) {
         Optional<String> dao = Arrays.stream(e.getStackTrace())
                 .map(StackTraceElement::getClassName)
-                .filter(className -> className.contains("Dao"))
+                .filter(className -> className.contains(afterName))
                 .findAny();
         return dao.get();
+    }
+
+    private String resolveResourceBundle(String key, Locale locale) {
+        if (!AVAILABLE_LOCALES.contains(locale.toString())) {
+            locale = DEFAULT_LOCALE;
+        }
+        return resourceBundleMessageSource.getMessage(key, null, locale);
     }
 
 }
