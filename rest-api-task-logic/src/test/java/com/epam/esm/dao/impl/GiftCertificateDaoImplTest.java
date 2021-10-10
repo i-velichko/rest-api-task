0f66b321ac3e83d1182670458ae49d6480b39058
Ivan_Velichko <showman.velichko@gmail.com>
2021-10-10 14:20:47 +0300

    added in-memory db settings for db tests

--- rest-api-task-logic/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
+++ rest-api-task-logic/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
@@ -1,12 +1,34 @@
 package com.epam.esm.dao.impl;
 
+import com.epam.esm.dao.config.TestConfig;
+import com.epam.esm.entity.GiftCertificate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {TestConfig.class})
 class GiftCertificateDaoImplTest {
+    private final GiftCertificateDaoImpl certificateDao;
+    private static GiftCertificate newGiftCertificate;
+
+    private static final long CERTIFICATE_ID = 1L;
+
+    @Autowired
+    public GiftCertificateDaoImplTest(GiftCertificateDaoImpl certificateDao) {
+        this.certificateDao = certificateDao;
+    }
 
     @BeforeEach
     void setUp() {
@@ -26,6 +48,8 @@ class GiftCertificateDaoImplTest {
 
     @Test
     void findById() {
+        Optional<GiftCertificate> actual = certificateDao.findById(CERTIFICATE_ID);
+        assertTrue(actual.isPresent());
     }
 
     @Test
---
+++ rest-api-task-logic/src/test/resources/db_setup.sql
@@ -0,0 +1,15 @@
+INSERT INTO gift_certificate (name, description, price, create_date, last_update_date, duration)
+VALUES ('certificate 1', 'description 1', 1.1, '2021-10-08 11:11:11', '2021-01-01 01:22:11', 1),
+       ('certificate 2', 'description 2', 2.2, '2021-10-08 11:11:11', '2021-01-01 01:22:11', 2),
+       ('certificate 3', 'description 3', 3.3, '2021-10-08 11:11:11', '2021-01-01 01:22:11', 3);
+
+INSERT INTO tag (name)
+VALUES ('i'),
+       ('want'),
+       ('work'),
+       ('epam');
+
+INSERT INTO certificates_tags (certificate_id, tag_id)
+VALUES (1, 1),
+       (1, 3),
+       (2, 2);
