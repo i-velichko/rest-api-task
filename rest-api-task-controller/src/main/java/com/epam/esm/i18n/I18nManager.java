3bc95b9ea162be4c32c378ae3166af375a5535ad
Ivan_Velichko <showman.velichko@gmail.com>
2021-10-15 12:00:05 +0300

    Move locale settings to property.

--- rest-api-task-controller/src/main/java/com/epam/esm/i18n/I18nManager.java
+++ rest-api-task-controller/src/main/java/com/epam/esm/i18n/I18nManager.java
@@ -1,6 +1,8 @@
 package com.epam.esm.i18n;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
@@ -17,10 +19,18 @@ import java.util.Locale;
  */
 
 @Component
+@PropertySource("classpath:locale.properties")
 public class I18nManager {
+    @Value("${locale.us}")
+    private String LOCALE_US;
+    @Value("${locale.ru}")
+    private String LOCALE_RU;
+    @Value("${language.en}")
+    private String LANGUAGE_EN;
+    @Value("${country.us}")
+    private String COUNTRY_US;
+
     private final ResourceBundleMessageSource resourceBundleMessageSource;
-    private static final List<String> AVAILABLE_LOCALES = Arrays.asList("en_US", "ru_RU");
-    private static final Locale DEFAULT_LOCALE = new Locale("en", "US");
 
     @Autowired
     public I18nManager(ResourceBundleMessageSource resourceBundleMessageSource) {
@@ -40,6 +50,8 @@ public class I18nManager {
     }
 
     public String getMessage(String key, Locale locale) {
+        List<String> AVAILABLE_LOCALES = Arrays.asList(LOCALE_US, LOCALE_RU);
+        Locale DEFAULT_LOCALE = new Locale(LANGUAGE_EN, COUNTRY_US);
         if (!AVAILABLE_LOCALES.contains(locale.toString())) {
             locale = DEFAULT_LOCALE;
         }
